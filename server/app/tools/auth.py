7e3f380990af155df145c396afa4ded32cad744d
Александр Окунев <frozz.way@gmail.com>
2025-05-17 20:56:27 +0500

    Добавил возможность ограничения ролей для auth tool

--- server/app/tools/auth.py
+++ server/app/tools/auth.py
@@ -4,8 +4,12 @@ from app.services import AuthService
 
 
 @cherrypy.tools.register('on_start_resource')
-def auth_tool():
-    """Tool для проверки авторизации пользователя в системе"""
+def auth_tool(authorized_roles: list[str] | None = None) -> None:
+    """
+    Tool для проверки авторизации пользователя в системе
+
+    :param authorized_roles: Роли, которыми ограничен доступ
+    """
 
     auth_header: str = cherrypy.request.headers.get('Authorization', '')
     if not auth_header:
@@ -15,4 +19,8 @@ def auth_tool():
     if token is None:
         raise AuthService.exception
 
-    AuthService.verify_token(token)
+    user = AuthService.verify_token(token)
+    user_roles = set(user.roles)
+
+    if authorized_roles and not any(authorized_role in user_roles for authorized_role in authorized_roles):
+        raise AuthService.exception
